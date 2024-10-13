c98eea9319db823c2888e43518320b4149005433
Adam-Doria <drici.adam@gmail.com>
2024-10-13 19:31:43 +0200

    feat: add theme toggle component

---
+++ antred-website/src/components/system/themeToggle/ThemeToggle.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import * as React from "react"
+import { Moon, Sun } from "lucide-react"
+import { useTheme } from "next-themes"
+
+import { Button } from "@/components/ui/button"
+import { useTranslations } from "next-intl"
+
+export function ThemeToggle() {
+    const { setTheme, theme } = useTheme()
+    const t = useTranslations("common")
+    return (
+        <Button
+            variant="ghost"
+            size="icon"
+            onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+        >
+            <Sun className="h-[1.5rem] w-[1.3rem] dark:hidden" />
+            <Moon className="hidden h-5 w-5 dark:block" />
+            <span className="sr-only"> {t('toggleTheme')}</span>
+        </Button>
+    )
+}
