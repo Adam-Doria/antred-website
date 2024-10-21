7a40447dfa6b981da23a340106c0eddefd2a80d7
Adam-Doria <drici.adam@gmail.com>
2024-10-21 20:51:22 +0200

    feat : add homepage hero section

--- antred-website/src/components/system/navbar/NavBar.tsx
+++ antred-website/src/components/system/navbar/NavBar.tsx
@@ -3,9 +3,9 @@ import { DesktopNavbar } from './DesktopNavbar'
 
 export const Navbar: React.FC = () => {
   return (
-    <>
+    <div className="z-50 fixed top-0 left-0 right-0 bg-background">
       <MobileNavbar />
       <DesktopNavbar />
-    </>
+    </div>
   )
 }
