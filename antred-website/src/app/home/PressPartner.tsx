aeefe6ad3c092975893b6d541700ceb48dc9f0db
Adam-Doria <drici.adam@gmail.com>
2024-10-24 22:28:36 +0200

    feat :press and partner

--- antred-website/src/app/home/PressPartner.tsx
+++ antred-website/src/app/home/PressPartner.tsx
@@ -1,11 +1,12 @@
 import { cn } from '@/lib/utils'
 import Marquee from '@/components/ui/marquee'
 import Image from 'next/image'
+import { useTranslations } from 'next-intl'
 
 const reviews = [
   {
-    name: 'blabla',
-    body: 'Sebastien Agrolard et son équipe se font un plaisir de vous accueillir du lundi au vendredi de 10h à 18h',
+    name: 'forenseek',
+    body: `A travers Forenseek, Sébastien Aguilar nous apporte son soutien et son oeil d\'expert de la Police Scientifique.`,
     img: 'forenseek.png',
     src: 'https://forenseek.fr'
   },
@@ -13,30 +14,31 @@ const reviews = [
     name: 'blabla',
     body: 'Association Unis pour Tiphaine',
     img: 'unispourtiphaine.png',
-    src: 'https://avatar.vercel.sh/james'
+    src: 'https://unispourtiphaine.org/'
   },
   {
     name: 'blabla',
-    body: 'regreg',
-    img: 'https://avatar.vercel.sh/john',
-    src: 'https://avatar.vercel.sh/james'
+    body: 'Ensemble pour retrouver Mathieu',
+    img: 'ensemblepourmathieu.png',
+    src: 'https://www.ensemblepourretrouvermathieu.com/fr/'
   },
   {
-    name: 'blabla',
-    body: 'titre de de larticle de presse ',
+    name: 'press1',
+    body: 'Unis pour Tiphaine et l’ANTRED pour la recherche de Français disparus à l’étranger ',
     img: 'lesfrancais.webp',
     src: 'https://lesfrancais.press/unis-pour-tiphaine-et-lantred-pour-la-recherche-de-francais-disparus-a-letranger/'
   },
   {
-    name: 'blabla',
-    body: 'ergg',
-    img: 'https://avatar.vercel.sh/jenny'
+    name: 'press2',
+    body: 'Unis pour Tiphaine et l’ANTRED pour la recherche de Français disparus à l’étranger ',
+    img: 'lesfrancais.webp',
+    src: 'https://lesfrancais.press/unis-pour-tiphaine-et-lantred-pour-la-recherche-de-francais-disparus-a-letranger/'
   },
   {
-    name: 'blabla',
-    body: 'ergeg',
-    img: 'https://avatar.vercel.sh/james',
-    src: 'https://avatar.vercel.sh/james'
+    name: 'press3',
+    body: 'Unis pour Tiphaine et l’ANTRED pour la recherche de Français disparus à l’étranger ',
+    img: 'lesfrancais.webp',
+    src: 'https://lesfrancais.press/unis-pour-tiphaine-et-lantred-pour-la-recherche-de-francais-disparus-a-letranger/'
   }
 ]
 
@@ -53,13 +55,6 @@ const ReviewCardDeux = ({
   src?: string
 }) => {
   return (
-    // <Image
-    //   height={64}
-    //   width={281}
-    //   // objectFit="contain"
-    //   src={`/images/presse/${img}`}
-    //   alt={`logo de ${img}`}
-    // />
     <a href={src}>
       <figure
         className={cn(
@@ -77,47 +72,7 @@ const ReviewCardDeux = ({
             src={`/images/presse/${img}`}
             alt={`logo de ${img}`}
           />
-          <blockquote className="mt-2 text-sm">{body}</blockquote>
-        </div>
-      </figure>
-    </a>
-  )
-}
-const ReviewCard = ({
-  img,
-  src
-}: {
-  img: string
-  name: string
-  body: string
-  src?: string
-}) => {
-  return (
-    // <Image
-    //   height={64}
-    //   width={281}
-    //   // objectFit="contain"
-    //   src={`/images/presse/${img}`}
-    //   alt={`logo de ${img}`}
-    // />
-    <a href={src}>
-      <figure
-        className={cn(
-          'relative w-64 cursor-pointer overflow-hidden rounded-xl border p-4 ',
-          // light styles
-          'border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]',
-          // dark styles
-          'dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]'
-        )}
-      >
-        <div className="flex flex-col items-center justify-center h-[120px]">
-          <Image
-            height={32}
-            width={281}
-            src={`/images/presse/${img}`}
-            alt={`logo de ${img}`}
-          />
-          {/* <blockquote className="mt-2 text-sm">{body}</blockquote> */}
+          <blockquote className="m-2 text-sm text-pretty">{body}</blockquote>
         </div>
       </figure>
     </a>
@@ -125,36 +80,30 @@ const ReviewCard = ({
 }
 
 export function PressAndPartner() {
+  const t = useTranslations('homepage.pressAndPartner')
   return (
     <>
-      <div className="relative flex h-[350px] w-full flex-col items-center justify-center overflow-hidden ">
+      <h2 className="mt-4"> {t('partnerTitle')} </h2>
+      <h4> {t('partnerSubtitle')}</h4>
+      <div className="relative flex h-[250] w-full flex-col items-center justify-center overflow-hidden my-8">
         <Marquee pauseOnHover className="[--duration:40s]">
           {Partner.map((review) => (
-            <ReviewCard key={review.name} {...review} />
+            <ReviewCardDeux key={review.name} {...review} />
           ))}
         </Marquee>
-        <Marquee reverse pauseOnHover className="[--duration:40s]">
-          {press.map((review) => (
-            <ReviewCard key={review.name} {...review} />
-          ))}
-        </Marquee>
-        <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-white dark:from-background"></div>
-        <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-white dark:from-background"></div>
+        <div className="pointer-events-none md:absolute md:inset-y-0 md:left-0 md:w-1/12 md:bg-gradient-to-r md:from-background/90 dark:from-background"></div>
+        <div className="pointer-events-none md:absolute md:inset-y-0 md:right-0 md:w-1/12 md:bg-gradient-to-l md:from-background/90 dark:from-background"></div>
       </div>
-      <h1>ou </h1>
-      <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden pb-8">
-        <Marquee pauseOnHover className="[--duration:40s]">
-          {Partner.map((review) => (
-            <ReviewCardDeux key={review.name} {...review} />
-          ))}
-        </Marquee>
+
+      <h2> {t('pressTitle')} </h2>
+      <div className="relative flex h-[250] w-full flex-col items-center justify-center overflow-hidden my-8">
         <Marquee reverse pauseOnHover className="[--duration:40s]">
           {press.map((review) => (
             <ReviewCardDeux key={review.name} {...review} />
           ))}
         </Marquee>
-        <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-white dark:from-background"></div>
-        <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-white dark:from-background"></div>
+        <div className="pointer-events-none md:absolute md:inset-y-0 md:left-0 md:w-1/12 md:bg-gradient-to-r md:from-background/90 dark:from-background"></div>
+        <div className="pointer-events-none md:absolute md:inset-y-0 md:right-0 md:w-1/12 md:bg-gradient-to-l md:from-background/90 dark:from-background"></div>
       </div>
     </>
   )
