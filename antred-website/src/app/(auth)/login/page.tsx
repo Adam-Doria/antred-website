3f79db515daaba3e84a1a7fd9e519d8abad7bb01
Adam-Doria <drici.adam@gmail.com>
2025-01-26 19:06:44 +0100

    feat(auth) : add supabase auth

--- antred-website/src/app/(auth)/login/page.tsx
+++ antred-website/src/app/(auth)/login/page.tsx
@@ -1,12 +1,13 @@
-'use client'
+// src/app/(auth)/login/page.tsx
+import { LoginForm } from '@/features/authentication/components/LoginForm'
 
-export default function Login() {
+export default function LoginPage() {
   return (
-    <div>
-      <h1>Login</h1>
-      <h1>Login</h1>
-      <h1>Login</h1>
-      <h1>Login</h1>
+    <div className="min-h-screen flex items-center justify-center bg-background">
+      <div className="w-full max-w-md p-8 space-y-4 bg-card rounded-lg shadow">
+        <h3>Connexion Admin</h3>
+        <LoginForm />
+      </div>
     </div>
   )
 }
