d358855ece2cf4164e47846bbf7e6ac97b511cbf
Adam-Doria <drici.adam@gmail.com>
2025-02-22 21:39:51 +0100

    chore: add redirect after login

--- antred-website/src/features/authentication/actions/auth.ts
+++ antred-website/src/features/authentication/actions/auth.ts
@@ -2,13 +2,14 @@
 
 import { createServerClient } from '@/lib/supabase/server'
 import { LoginFormValues } from '../schemas/auth'
+import { redirect } from 'next/navigation'
 
 export const login = async (formData: LoginFormValues) => {
   const supabase = await createServerClient()
 
   const { error } = await supabase.auth.signInWithPassword(formData)
-  console.log(error)
   if (error) return { error: error.message }
+  redirect('/admin')
 
   return { success: 'succes' }
 }
