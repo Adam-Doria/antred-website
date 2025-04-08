39362b9aeec6ef5e516e6602453eb640892cbf25
Adam-Doria <drici.adam@gmail.com>
2025-04-08 11:19:59 +0200

    fix: remove useless envconfig

--- src/features/missingPersons/actions/index.ts
+++ src/features/missingPersons/actions/index.ts
@@ -1,4 +1,7 @@
 export { createMissingPerson } from './mutations/createMissingPerson'
+export { updateMissingPerson } from './mutations/updateMissingPerson'
+export { deleteMissingPerson } from './mutations/deleteMissingPerson'
+
 export {
   getMissingPerson,
   getMissingPersonById
--- src/lib/database/db.ts
+++ src/lib/database/db.ts
@@ -1,7 +1,6 @@
 import { CamelCasePlugin, Kysely, PostgresDialect } from 'kysely'
 import { Database } from './types'
 import { Pool } from 'pg'
-import '../envConfig.ts'
 
 let databaseInstance: Kysely<Database> | null = null
 
